a09003b308942a787f5e8ac4cdc9839bc6e47267
kartikay Kala <kartikay304@gmail.com>
2021-12-02 21:13:59 +0530

    added pages to the navbar

--- app/controllers/blooddonation_controller.rb
+++ app/controllers/blooddonation_controller.rb
@@ -1,4 +1,14 @@
 class BlooddonationController < ApplicationController
-    def index
+    def index 
+    end
+    def plasama 
+    end
+    def why_give_blood 
+    end
+    def who_can_give_blood
+    end
+    def donation_process 
+    end
+    def about 
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "blooddonation#index"
+  root "blooddonation#index"
+  get "/plasama", to: "blooddonation#plasama"
+  get "/why_give_blood", to: "blooddonation#why_give_blood"
+  get "/who_can_give_blood", to: "blooddonation#who_can_give_blood"
+  get "/donation_process", to: "blooddonation#donation_process"
+  get "/about", to: "blooddonation#about"
 
 end
