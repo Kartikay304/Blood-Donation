11be2fb31447a7a443cab3b9ffc96c754c1b10ce
kartikay Kala <40604582+Kartikay304@users.noreply.github.com>
2021-12-25 21:39:18 +0530

    completed home page

--- app/controllers/blooddonation_controller.rb
+++ app/controllers/blooddonation_controller.rb
@@ -1,15 +1,15 @@
 class BlooddonationController < ApplicationController
     def index
+        @cores = [{ name: "Kartikay Kala", img: "https://avatars.githubusercontent.com/u/40604582?v=4", link: "https://github.com/Kartikay304" }]
     end
 
     def create
         @post=Post.new(post_params)
-        @post.save
         if @post.save
             flash[:notice] = "Request send successfully."
             redirect_to root_path
         else
-            flash[:notice] = "Something went wrong!,Re-check the details in form"
+            flash[:notice] = "Something went wrong! Re-check the details filled in the form"
             redirect_to root_path
         end
     end
