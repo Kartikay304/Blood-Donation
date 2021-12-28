cb102816c67d4db911654ab77d325679b0a0d3b2
kartikay Kala <40604582+Kartikay304@users.noreply.github.com>
2021-12-28 13:27:02 +0530

    completed blood-withdraw

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,19 +1,18 @@
 class DashboardController < ApplicationController
 
     def index
-        @A=Userdonation.where(blood_group: 'A+').sum(:donated)
-        @a=Userdonation.where(blood_group: 'A-').sum(:donated)
-        @B=Userdonation.where(blood_group: 'B+').sum(:donated)
-        @b=Userdonation.where(blood_group: 'B-').sum(:donated)
-        @AB=Userdonation.where(blood_group: 'AB+').sum(:donated)
-        @ab=Userdonation.where(blood_group: 'AB-').sum(:donated)
-        @O=Userdonation.where(blood_group: 'O+').sum(:donated)
-        @o=Userdonation.where(blood_group: 'O-').sum(:donated)
+        @A=Userdonation.where(blood_group: 'A+').sum(:donated)-Userdonation.where(blood_group: 'A+').sum(:received)
+        @a=Userdonation.where(blood_group: 'A-').sum(:donated)-Userdonation.where(blood_group: 'A-').sum(:received)
+        @B=Userdonation.where(blood_group: 'B+').sum(:donated)-Userdonation.where(blood_group: 'B+').sum(:received)
+        @b=Userdonation.where(blood_group: 'B-').sum(:donated)-Userdonation.where(blood_group: 'B-').sum(:received)
+        @AB=Userdonation.where(blood_group: 'AB+').sum(:donated)-Userdonation.where(blood_group: 'AB+').sum(:received)
+        @ab=Userdonation.where(blood_group: 'AB-').sum(:donated)-Userdonation.where(blood_group: 'AB-').sum(:received)
+        @O=Userdonation.where(blood_group: 'O+').sum(:donated)-Userdonation.where(blood_group: 'O+').sum(:received)
+        @o=Userdonation.where(blood_group: 'O-').sum(:donated)-Userdonation.where(blood_group: 'O-').sum(:received)
     end
 
     def create
         @userdonation=Userdonation.new(userdonation_params)
-        @userdonation.save
         if @userdonation.save
             flash[:notice] = "#{@userdonation.username}, Donation request is send to the admin"
             redirect_to dashboard_path(Current.user.id)
